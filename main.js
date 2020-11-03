5b69d638373fabe64fdb1ea30b73d746384b6ac8
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-03 15:27:05 +0100

    added month

--- main.js
+++ main.js
@@ -23,6 +23,38 @@ function upDateClock() {
     //update weekday
     const weekdayHolder = document.getElementById('weekday');
     weekdayHolder.innerText = getWeekday(date);
+
+    //update date
+    const monthHolder = document.getElementById('month')
+    monthHolder.innerText = getMonthString(date);
+}
+
+/**
+ * 
+ * @param {date} date 
+ */
+function getMonthString(date) {
+    const dayOfMonth = date.getDate();
+    const monthIndex = date.getMonth();
+
+    const prefix = dayOfMonth + ' ';
+
+    switch (monthIndex) {
+        case 0: return prefix + 'Januari';
+        case 1: return prefix + 'Februari';
+        case 2: return prefix + 'Mars';
+        case 3: return prefix + 'April';
+        case 4: return prefix + 'Maj';
+        case 5: return prefix + 'Juni';
+        case 6: return prefix + 'Juli';
+        case 7: return prefix + 'Augusti';
+        case 8: return prefix + 'September';
+        case 9: return prefix + 'Oktober';
+        case 10: return prefix + 'November';
+        case 11: return prefix + 'December';
+
+    }
+    
 }
 
 function getWeekday(date) {
