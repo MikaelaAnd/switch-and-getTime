5b7cb1c1411a2b33861f52ba8e3ef8d48b40f338
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-03 14:35:20 +0100

    added time

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const date = new Date()
-console.log(date.toDateString())
-console.log(date.getFullYear())
+// window.addEventListener("load", startTime)
 
-window.addEventListener("load", startTime)
+// const date = new Date()
+// console.log(date.toDateString())
+// console.log(date.getFullYear())
 
-function startTime() {
-    var currentTime = new Date();
-    document.getElementById('time').innerHTML = currentTime.toDateString();
-    console.log('lol')
+
+// function startTime() {
+//     var currentTime = new Date();
+//     document.getElementById('time').innerHTML = currentTime.toDateString();
+//     console.log('lol')
+// }
+
+
+window.onload = main;
+
+function main() {
+    setInterval(upDateClock, 1000);
+    
+}
+
+function upDateClock() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const timeHolder = document.getElementById('time');
+
+    timeHolder.innerText = hours + ':' + minutes + ':' + seconds;
 }
