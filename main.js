cb44eda0ccdb321479d04ccace4333439ce733a9
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-03 14:41:27 +0100

    added more time

--- main.js
+++ main.js
@@ -20,11 +20,23 @@ function main() {
 }
 
 function upDateClock() {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
     const timeHolder = document.getElementById('time');
-
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+    
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+    
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+    
     timeHolder.innerText = hours + ':' + minutes + ':' + seconds;
 }
